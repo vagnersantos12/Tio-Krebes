a8d143a7dbb2f8134dc45982c24b31dd9e0ed2ef
Vagner Santos <67028371+vagnersantos12@users.noreply.github.com>
2023-01-31 18:25:39 -0300

    banco de dados em php

--- contato.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$nome =
-$sobrenome 
-$email
-$telefone 
-
-?>
---
+++ src/contato.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Twilio\Rest\Client;
+
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
+$twilioSid    = getenv('TWILIO_SID');
+$twilioToken  = getenv('TWILIO_TOKEN');
+
+$twilio = new Client($twilioSid, $twilioToken);
+
+$message = $twilio->messages
+                 ->create(
+                       "whatsapp:+558798133618",
+                     array(
+                              "body" => "Greetings from Twilio :-)",
+                              "from" => "whatsapp:+558798133618"
+                          )
+                 );
+<?;
